a03e18c30e78bda5a28624e95e5b44c4d25c1973
Abhishyant Khare <Abhishyant Khare>
2018-12-08 16:01:32 -0800

    added b

--- buckler_telnet.py
+++ buckler_telnet.py
@@ -7,7 +7,7 @@ class BucklerTelnet:
   def __init__(self):
     self.telnet_conn = telnetlib.Telnet("localhost", 19021)
     # Clear initial output on telnet channel
-    self.telnet_conn.read_until("JLinkExe\r\n") 
+    self.telnet_conn.read_until(b"JLinkExe\r\n") 
     self.stop_set = False
 
   def setSpeed(self, newSpeed):
@@ -17,7 +17,7 @@ class BucklerTelnet:
     if self.stop_set:
       self.stop()
       return 
-    self.telnet_conn.write("setSpeed")
+    self.telnet_conn.write(b"setSpeed")
     self.telnet_conn.write(newSpeed)
   
   def setTurnSpeed(self, newSpeed):
@@ -27,7 +27,7 @@ class BucklerTelnet:
     if self.stop_set:
       self.stop()
       return 
-    self.telnet_conn.write("turn spd")
+    self.telnet_conn.write(b"turn spd")
     self.telnet_conn.write(newSpeed)
 
   def driveInfinite(self):
@@ -37,10 +37,10 @@ class BucklerTelnet:
     if self.stop_set:
       self.stop()
       return 
-    self.telnet_conn.write("driveInf")
+    self.telnet_conn.write(b"driveInf")
   
   def stop(self):
-    self.telnet_conn.write("stop")
+    self.telnet_conn.write(b"stop")
 
   def driveDist(self, dist):
     msg = self.telnet_conn.read_eager()
@@ -49,8 +49,8 @@ class BucklerTelnet:
     if self.stop_set:
       self.stop()
       return 
-    self.telnet_conn.write("driveDis")
-    self.telnet_conn.write("%.2f" % dist)
+    self.telnet_conn.write(b"driveDis")
+    self.telnet_conn.write(b"%.2f" % dist)
     self.blockUntilDone()
   
   def turnRightInfinite(self):
@@ -60,7 +60,7 @@ class BucklerTelnet:
     if self.stop_set:
       self.stop()
       return 
-    self.telnet_conn.write("rightInf")
+    self.telnet_conn.write(b"rightInf")
   
   def turnLeftInfinite(self):
     msg = self.telnet_conn.read_eager()
@@ -69,7 +69,7 @@ class BucklerTelnet:
     if self.stop_set:
       self.stop()
       return 
-    self.telnet_conn.write("left Inf")
+    self.telnet_conn.write(b"left Inf")
   
   def turnRightAngle(self, angle):
     msg = self.telnet_conn.read_eager()
@@ -78,8 +78,8 @@ class BucklerTelnet:
     if self.stop_set:
       self.stop()
       return 
-    self.telnet_conn.write("rightAng")
-    self.telnet_conn.write("%.2f" % angle)
+    self.telnet_conn.write(b"rightAng")
+    self.telnet_conn.write(b"%.2f" % angle)
     self.blockUntilDone()
   
   def turnLeftAngle(self, angle):
@@ -89,8 +89,8 @@ class BucklerTelnet:
     if self.stop_set:
       self.stop()
       return 
-    self.telnet_conn.write("leftAngl")
-    self.telnet_conn.write("%.2f" % angle)
+    self.telnet_conn.write(b"leftAngl")
+    self.telnet_conn.write(b"%.2f" % angle)
     self.blockUntilDone()
   
   def reverseInfinite(self):
@@ -100,7 +100,7 @@ class BucklerTelnet:
     if self.stop_set:
       self.stop()
       return 
-    self.telnet_conn.write("reverInf")
+    self.telnet_conn.write(b"reverInf")
   
   def reverseDist(self, dist):
     msg = self.telnet_conn.read_eager()
@@ -109,24 +109,24 @@ class BucklerTelnet:
     if self.stop_set:
       self.stop()
       return 
-    self.telnet_conn.write("reverDis")
-    self.telnet_conn.write("%.2f" % dist)
+    self.telnet_conn.write(b"reverDis")
+    self.telnet_conn.write(b"%.2f" % dist)
     self.blockUntilDone()
 
   def resetGrabber(self):
-    self.telnet_conn.write("rst_grab")
+    self.telnet_conn.write(b"rst_grab")
     time.sleep(3)
   
   def rotateGrabber(self):
-    self.telnet_conn.write("grabserv")
+    self.telnet_conn.write(b"grabserv")
     time.sleep(3)
   
   def liftCup(self):
-    self.telnet_conn.write("liftserv")
+    self.telnet_conn.write(b"liftserv")
     time.sleep(3)
   
   def resetLift(self):
-    self.telnet_conn.write("rst_lift")
+    self.telnet_conn.write(b"rst_lift")
     time.sleep(3)
   
   def blockUntilDone(self):
